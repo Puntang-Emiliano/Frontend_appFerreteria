19546d35b23ef247df79dcf3a2e846e5c81435a1
tomasbasso <tomas.basso@hotmail.com>
2024-10-10 21:12:48 -0300

    Cargar Usuarios

--- AppStore/mvvm/Views/HomePage.xaml.cs
+++ AppStore/mvvm/Views/HomePage.xaml.cs
@@ -4,9 +4,16 @@ namespace AppStore.mvvm.Views
 {
     public partial class HomePage : ContentPage
     {
+        // Constructor que recibe un HomeViewModel
         public HomePage(HomeViewModel viewModel)
         {
-            BindingContext = viewModel;
+            InitializeComponent();
+            BindingContext = viewModel; // Establece el BindingContext a viewModel
+        }
+
+        // Constructor sin parámetros (puedes mantenerlo si lo necesitas)
+        public HomePage()
+        {
             InitializeComponent();
         }
     }
